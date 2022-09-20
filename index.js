d17d62e4a925abe71beb1a70e1984194dd1f1bf1
José Luis García <104162482+jose-luis-rocketcode@users.noreply.github.com>
2022-09-20 15:40:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+
+const sproc = require('./sprocs/sproc_main')
+
+app.get('/sp_get_message', async (req, res) => {
+    try{
+        const name = req.query.name
+        let id_range = req.query.phone
+        id_range = id_range.split(',')
+        id_range = id_range.map(x=>parseInt(x))
+
+        const result = await sproc.run_sp('SP_GETMESSAGE',[name,id_range],res)
+
+        return res.status(200).json(result[0])
+    }catch(err){
+        return res.status(400).json({err:err.message})
+    }
+});
+
+
+//Refresh store procedures at app starting
+((response = null)=>{
+try {
+    console.log("[index][sproc_init] start");
+
+    const sprocs_init = require('./sprocs/sprocs_class/sproc_init')
+    sprocs_init(response)
+
+} catch(err) {
+    console.log("[index][sproc_init] err"+err.message);
+}
+})()
+
+
+server.listen(process.env.PORT || 8080,()=>{
+    console.log('server running...at 8080');
+});
